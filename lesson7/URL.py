857f1710293f80b4be54c7071a407af03365f3f9
LilitAkinshina <164274892+LilitAkinshina@users.noreply.github.com>
2024-07-19 23:00:54 +0300

    Delete lesson7 directory

--- lesson7/URL.py
+++
@@ -1,5 +0,0 @@
-form_url = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
-
-calculator_url = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
-
-shop_url = "https://www.saucedemo.com/"
