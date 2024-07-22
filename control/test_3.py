eee745127b7befad2686f5a985f2846ac62a5a18
LilitAkisnhina <lilitbl4@rambler.ru>
2024-07-22 21:39:24 +0300

    control.1

--- control/test_3.py
+++ control/test_3.py
@@ -1,35 +1,40 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome()
 
-driver.get("http://www.saucedemo.com/")
+@pytest.fixture(scope="module")
+def setup():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.get("https://www.saucedemo.com/")
+    yield driver
+    driver.quit()
 
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
-driver.find_element(By.ID, "password").send_keys("secret_sauce")
-driver.find_element(By.ID, "login-button").click()
+def test_shop(setup):
+    driver = setup
 
-driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
 
-driver.find_element(By.ID, "shopping_cart_container").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+    driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
 
-driver.find_element(By.ID, "checkout").click()
+    driver.find_element(By.ID, "shopping_cart_container").click()
 
-driver.find_element(By.ID, "first-name").send_keys("Лилит")
-driver.find_element(By.ID, "last-name").send_keys("Акиншина")
-driver.find_element(By.ID, "postal-code").send_keys("333666")
+    driver.find_element(By.ID, "checkout").click()
 
-driver.find_element(By.ID, "continue").click()
+    driver.find_element(By.ID, "first-name").send_keys("Лилит")
+    driver.find_element(By.ID, "last-name").send_keys("Акиншина")
+    driver.find_element(By.ID, "postal-code").send_keys("333666")
 
-total_price = driver.find_element(By.CLASS_NAME, 'summary_total_label')
+    driver.find_element(By.ID, "continue").click()
 
-driver.quit()
-
-total = total_price.text.strip().replace("Total: $", "")
-excepted_total = "58.29"
-assert total == excepted_total
-print("Итого $58.29")
+    total_price = driver.find_element(By.CLASS_NAME, 'summary_total_label')
+    total = total_price.text.strip().replace("Total: $", "")
+    excected_total = "58.29"
+    assert total == excected_total
+    print(f"Итого ${total}")
