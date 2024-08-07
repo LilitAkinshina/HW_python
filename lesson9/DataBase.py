e9b0a8a434eac33cb5f3f2f7026bccf8523bdae4
LilitAkisnhina <lilitbl4@rambler.ru>
2024-08-07 14:42:19 +0300

    lesson7.1

--- lesson9/DataBase.py
+++ lesson9/DataBase.py
@@ -1,117 +1,84 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 
 
-class DataBase:
-    query = {
-        'create_company': text('insert into company (name, description) values (:name, :description)'),
-        'max_company_id': text('select MAX(id) from company'),
-        'delete_company_id': text('delete from company where id = :company_id'),
-        'list_SELECT': text('select * from employee where company_id = :id'),
-        'item_SELECT': text('select * from employee where company_id = :c_id and id = :e_id'),
-        'maxID_SELECT': text('select MAX(id) from employee where company_id = :c_id'),
-        'item_DELETE': text('delete from employee where id = :id_delete'),
-        'item_UPDATE': text('update employee set first_name = :new_name where id = :employer_id'),
-        'item_INSERT': text('insert into employee(company_id, first_name, last_name, phone) values(:id, :name, :surname, :phone_num)')
+class EmployeeTable:
+    scripts = {
+        "delete employee": text("DELETE FROM employee "
+                                "WHERE id = :new_employee_id"),
+        "get company's employees": text("SELECT * FROM employee "
+                                        "WHERE company_id = :company_id"),
+        "get employee by id": text("SELECT * FROM employee "
+                                   "WHERE id = :employee_id"),
+        "get max id": text("SELECT MAX(id) FROM employee"),
+        "insert new": text("INSERT INTO employee(first_name, last_name, "
+                           "phone, company_id, is_active) "
+                           "VALUES (:first_name, :last_name, :phone, "
+                           ":company_id, :is_active)"),
+        "update employee": text("UPDATE employee "
+                                "SET email = :new_email, "
+                                "avatar_url = :new_url, "
+                                "is_active = :new_is_active "
+                                "WHERE id = :new_employee_id")
     }
 
-    def __init__(self, engine):
-        self.db = create_engine(engine)
+    def __init__(self, db_connection_string):
+        self.db = create_engine(db_connection_string)
 
-    def create_company(self, company_name: str, description: str):
-        try:
-            with self.db.connect() as connection:
-                result = connection.execute(self.query['create_company'], parameters=dict(name=company_name, description=description))
-                connection.commit()
-            return result
-        except Exception as _ex:
-            print("[INFO] Error - can't work with SQL", _ex)
-        finally:
-            if connection:
-                connection.close()
-                print("[INFO] DB connection closed")
+    def create(self, first_name, last_name, phone, company_id, is_active):
+        new_employee = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "phone": phone,
+            "company_id": company_id,
+            "is_active": is_active
+        }
+        self.db.execute(self.scripts["insert new"], new_employee)
 
-    def delete(self, company_id: int):
-        try:
-            with self.db.connect() as connection:
-                connection.execute(self.query['delete_company_id'], parameters=dict(company_id=company_id))
-                connection.commit()
-        except Exception as _ex:
-            print("[INFO] Error - can't work with SQL", _ex)
-        finally:
-            if connection:
-                connection.close()
-                print("[INFO] DB connection closed")
+    def delete(self, id):
+        self.db.execute(self.scripts["delete employee"], new_employee_id=id)
 
-    def last_company_id(self):
-        try:
-            with self.db.connect() as connection:
-                result = connection.execute(self.query['max_company_id']).fetchall()[0][0]
-                return result
-        except Exception as _ex:
-            print("[INFO] Error - can't work with SQL", _ex)
-        finally:
-            if connection:
-                connection.close()
-                print("[INFO] DB connection closed")
+    def get_company_employees(self, new_company_id):
+        return self.db.execute(self.scripts["get company's employees"],
+                               company_id=new_company_id).fetchall()
 
-    def get_list_employer(self, company_id: int):
-        try:
-            with self.db.connect() as connection:
-                result = connection.execute(self.query['list_SELECT'], parameters=dict(id=company_id)).fetchall()
-                return result
-        except Exception as _ex:
-            print("[INFO] Error - can't work with SQL", _ex)
-        finally:
-            if connection:
-                connection.close()
-                print("[INFO] DB connection closed")
+    def get_employee_by_id(self, id):
+        return self.db.execute(self.scripts["get employee by id"],
+                               employee_id=id).fetchall()
 
-    def create_employer(self, company_id: int, first_name: str, last_name: str, phone: str):
-        try:
-            with self.db.connect() as connection:
-                result = connection.execute(self.query['item_INSERT'], parameters=dict(id=company_id, name=first_name, surname=last_name, phone_num=phone))
-                connection.commit()
-                return result
-        except Exception as _ex:
-            print("[INFO] Error - can't work with SQL", _ex)
-        finally:
-            if connection:
-                connection.close()
-                print("[INFO] DB connection closed")
+    def get_max_id(self):
+        return self.db.execute(self.scripts["get max id"]).fetchall()[0][0]
 
-    def get_employer_id(self, company_id: int):
-        try:
-            with self.db.connect() as connection:
-                result = connection.execute(self.query['maxID_SELECT'], parameters=dict(c_id=company_id)).fetchall()[0][0]
-                return result
-        except Exception as _ex:
-            print("[INFO] Error - can't work with SQL", _ex)
-        finally:
-            if connection:
-                connection.close()
-                print("[INFO] DB connection closed")
+    def update(self, id, email, url, is_active):
+        self.db.execute(self.scripts["update employee"], new_email=email,
+                        new_url=url, new_is_active=is_active,
+                        new_employee_id=id)
 
-    def update_employer_info(self, new_name: str, id: int):
-        try:
-            with self.db.connect() as connection:
-                result = connection.execute(self.query['item_UPDATE'], parameters=dict(new_name=new_name, employer_id=id))
-                connection.commit()
-        except Exception as _ex:
-            print("[INFO] Error - can't work with SQL", _ex)
-        finally:
-            if connection:
-                connection.close()
-                print("[INFO] DB connection closed")
 
-    def delete_employer(self, id: int):
-        try:
-            with self.db.connect() as connection:
-                result = connection.execute(self.query['item_DELETE'], parameters=dict(id_delete=id))
-                connection.commit()
-                return result
-        except Exception as _ex:
-            print("[INFO] Error - can't work with SQL", _ex)
-        finally:
-            if connection:
-                connection.close()
-                print("[INFO] DB connection closed")
+class CompanyTable:
+    __scripts = {
+        "select": 'select * from company where deleted_at is null',
+        "select_only_active": 'select * from company '
+                              'where deleted_at is null '
+                              'and is_active = true',
+        "delete_by_id": text("DELETE FROM company WHERE id = :id_to_delete"),
+        "insert new": text("INSERT INTO company(\"name\", description) "
+                           "VALUES (:new_name, :new_description)"),
+        "get_max_id": text("SELECT MAX(id) FROM company"),
+        "select_by_id": text('SELECT * FROM company '
+                             'WHERE id = :select_id '
+                             'AND deleted_at is null')
+    }
+
+    def __init__(self, db_connection_string):
+        self.db = create_engine(db_connection_string)
+
+    def delete(self, id):
+        self.db.execute(self.__scripts["delete_by_id"], id_to_delete=id)
+
+    def create(self, name, description):
+        self.db.execute(self.__scripts["insert new"], new_name=name,
+                        new_description=description)
+
+    def get_max_id(self):
+        return self.db.execute(self.__scripts["get_max_id"]).fetchall()[0][0]
